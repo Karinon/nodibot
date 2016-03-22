85a28732ecebb59da5164d1b63114d01571139f1
Andrej Fast <andrejfast@googlemail.com>
2016-03-22 11:03:56 +0100

    Splitted nodibot into modules, implemented some minor stuff

--- nodi.js
+++ nodi.js
@@ -1,11 +1,11 @@
 #!/usr/local/bin/node
 
 var config = {
-    userName: "nodi",
+    userName: "Rainer",
     realName: "geh weg",
-    channels: ["#stipkas"],
+    channels: ["#clafoutis"],
     server: "irc.iz-smart.net",
-    botName: "nodi",
+    botName: "Rainer",
     port: 6697,
     secure: true,
     selfSigned: true,
@@ -15,243 +15,116 @@ var config = {
 
 var sleep = require('sleep');
 var irc = require('irc');
-var request = require('request');
+var drama = require("./drama.js");
+var weather = require("./weather.js");
+var onlineRequests = require("./onlineRequests.js");
 
-var verwirrtes = ["ups", "oh hi", "oh oh...", "na sowas", "oh.. du warst nicht gemeint", "oh.. hi", "argh, mist", "oh scheisse", "oh verdammt", "wenn man vom teufel spricht", "gutes timing", "hm, das ist jetzt unangenehm", "oh, peinlich.."];
-var gruesse = ["hi", "hallo", "tag", "moin", "mahlzeit", "tach", "hallo fans"];
-var boeseabschiede = ["und tschüss", "na endlich", "plötzlich riechts hier viel besser", "na also", "endlich weg", "auf nimmerwiedersehen", "dieser typ...", "ja, hau doch ab", "ja, geh doch", "genau, raus hier"];
-var liebeabschiede = ["och :(", "verlass mich noch nicht :(", "schade", "oh nein :(", "bleib doch hier!", "wohin gehst du so schnell? :(", "ach schade", "den mag ich", "neeeein :("];
-
-
-var beleidigungen = [
-    " ist so ein depp",
-    " ist so eine kleine dramaqueen",
-    " ist ein schlappschwanz",
-    " hat hier nichts beizutragen",
-    " soll sich endlich verpissen",
-    " ist eine ziemliche aufmerksamkeitshure",
-    " ist ein ziemliches stück dreck, warum ist er noch hier?",
-    " nervt",
-    " ist ein wertloser Gnom",
-    " keiner will den hier haben, aber sag ihm das nicht",
-    " ist hier nicht sonderlich beliebt",
-    " ist immer so",
-    " weiß aber nichts davon?",
-    " ist echt ein versager",
-    " darf das aber nicht hören",
-    " ist so ein spast",
-    " ist ne fotze",
-    " kann echt nichts",
-    " hat sie nicht mehr alle",
-    " hat wahrscheinlich psychische probleme",
-    " kann mich mal kreuzweise",
-    " mangelt es an esprit",
-    " hat hier gar nichts zu melden",
-    " braucht gar nicht mehr angekrochen zu kommen"
-]; 
-
-var lobe = [
-    " ist ganz nett",
-    " ist echt intelligent",
-    " ist lieb",
-    " mag fast jeder",
-    " ist hier recht beliebt",
-    " ist ziemich gebildet",
-    " sieht glaube ich ganz hübsch aus",
-    " ist zu gut für diese welt",
-    " wird es noch weit bringen",
-    " hat da wirklich talent",
-    " ist ziemlich sexy",
-    " ist wunderschön",
-    " kann echt nett sein"
-];
-
-var eroeffnung = [
-    "ja, ",
-    "jo ",
-    "absolut, ",
-    "ernsthaft, ",
-    "stimme 100%% zu, ",
-    "volle zustimmung, ",
-    "jeder wird mir da recht geben, ",
-    "ja, der meinte neulich auch, ",
-    "ich stimme da zu, ",
-    "richtig, ",
-    "korrekt, ",
-    "absolut richtig, ",
-    "wie gesagt, ",
-    "da stimme ich absolut zu, ",
-    "das sagen alle: "
-];
-
-var reaktionboese = [
-    "hat jemand die null gewählt?",
-    "was labert die denn schon wieder?",
-    "so ein blödsinn",
-    "totaler quatsch",
-    "bist du wahnsinnig?",
-    "gäääähn",
-    "hackts?",
-    "gehts noch?",
-    "wie langweilig",
-    "ach komm, das will keiner lesen",
-    "ich kann mir das nicht länger durchlesen",
-    "kann dem mal einer das maul stopfen?",
-    "wir sind hier nicht bei knuddels...",
-    "ruhe jetzt",
-    "schnauze!",
-    "es kann sprechen...",
-    "immer der selbe blödsinn",
-    "pff, blödsinn",
-    "halt doch endlich die fresse",
-    "schwachsinn",
-    "was redest du so ein blech?",
-    "kannst du auch mal was intelligentes sagen?",
-    "keiner mag dich...",
-    "ratte...",
-    "schwein...",
-    "-.-",
-    "D:",
-];
-
-var reaktionlieb = [
-    "stimmt",
-    "du bist so klug!",
-    "wow, so hab ich das noch nie gesehen :)",
-    "du weißt echt viel",
-    "haaach *_*",
-    "du bist echt toll",
-    "ich will dich nie verlieren :)",
-    "hoffentlich bleibst du noch lange :)",
-    "du bist so schön :)",
-    "du bist so lieb <3",
-    "erzähl mir mehr davon :)",
-    "du bist einer der wenigen vernünftigen hier :)",
-    "ich glaube ich mag dich :)"
-];
-
-var laune = -10;
+var warnings = require("./resources/warnhinweysze.json");
+var boa = require("./resources/boarisch.json");
+var drachenlord = require("./resources/drachen.json");
 
 var bot = new irc.Client(config.server, config.botName, {
     channels: config.channels
 });
+
 bot.addListener("registered", function(message) {
     console.log('Connected');
 });
+
 bot.addListener("motd", function(motd) {
     console.log('MOTD received');
 });
 
-function verbalLaune(laune) {
-    if (laune < 0) {
-        return "schlecht";
-    } else {
-        return "gut";
-    }
-}
-
-function verabschieden(ziel) {
-    var byebye = boeseabschiede[Math.floor(Math.random() * boeseabschiede.length)];
-    var adieu = liebeabschiede[Math.floor(Math.random() * liebeabschiede.length)];
-    if (laune < 0) {
-        bot.say(ziel, byebye);
-    } else {
-        bot.say(ziel, adieu);
-    }
-}
-
-function begruessen(ziel, person) {
-    var beleidigung = beleidigungen[Math.floor(Math.random() * beleidigungen.length)];
-    var liebe = lobe[Math.floor(Math.random() * lobe.length)];
-    var start = eroeffnung[Math.floor(Math.random() * eroeffnung.length)];
-    var oops = verwirrtes[Math.floor(Math.random() * verwirrtes.length)];
-    switch (person) {
-        case config.botName:
-            console.log('successfully joined');
-            break;
-
-        default:
-            if (Math.random() > 0.5) {
-                if (laune < 0) {
-                    sleep.sleep(2);
-                    bot.say(ziel, start + person + beleidigung);
-                    sleep.sleep(3);
-                    bot.say(ziel, oops);
-                    laune = laune + (100 * Math.random());
-                } else {
-                    bot.say(ziel, start + person + liebe);
-                    laune = laune - (100 * Math.random());
-                }
-                break;
-            }
-    }
-}
-
-function getRandomWikiTitle(prefix, callback) {
-  request('http://de.wikipedia.org/wiki/Spezial:Zuf%C3%A4llige_Seite', function (error, response, body) {
-    if (!error && response.statusCode == 200) {
-      var regex = '.*<title>(.*) – Wikipedia</title>.*'
-      var result = body.match(regex);
-      var statusNumber = result[1];
-      return callback(prefix, statusNumber);
-    }
-    return '';
-  });
-}
-
-function reagieren(ziel, nachricht) {
-    var regex = '!begruesze (.*)'
-    var result = nachricht.match(regex);
-    if(result != null) {
-      var name = result[1];
-      var greeting = 'TT' + name.substring(0,1) + ', SIE '
-      getRandomWikiTitle(greeting, function(prefix, title) {
+function isPhrase(ziel, nachricht) {
+    if(nachricht.startsWith("!sprichwort")) {
+      onlineRequests.getPhrase(function(phrase) {
         sleep.sleep(1);
-        bot.say(ziel, prefix.toUpperCase() + title.toUpperCase() + '!');
-        console.log(prefix.toUpperCase() + title + '!');
+        bot.say(ziel, phrase);
+        console.log(phrase);
       });
+      return true;
+    }
+    return false;
+}
+
+function greeting(to, message) {
+  var res = message.split(/[\s,]+/).filter(function(n) { return n.trim() != ''}); 
+  if(res.length == 0) {
+    bot.say(to, "Ich glaube ich lebe in einer Realität, die du in deinem ganzen Leben nicht begreifen wirst");
+  }
+  for(var i = 0; i < res.length; i++) {
+    var isBoarisch = Math.floor((Math.random() * 2));
+    console.log("1: " + isBoarisch);
+    var greeting = 'TT' + res[i].substring(0,1) + ', ';
+    if (isBoarisch > 0) {
+      greeting = (greeting + "SE ").toUpperCase();
+      var index = Math.floor((Math.random() * (boa.list.length)));  
+      var title = boa.list[index].toUpperCase();
+      bot.say(to, greeting + title + '!');
+      console.log(greeting + title + '!');
     } else {
-      var antwortboese = reaktionboese[Math.floor(Math.random() * reaktionboese.length)];
-      var antwortlieb = reaktionlieb[Math.floor(Math.random() * reaktionlieb.length)];
-
-      if (Math.random() < 0.1) {
-        if (laune < 0) {
-          if (laune < 0) {
-            sleep.sleep(2);
-            bot.say(ziel, antwortboese);
-            laune = laune + (100 * Math.random());
-          } else {
-            sleep.sleep(2);
-            bot.say(ziel, antwortlieb);
-            laune = laune - (100 * Math.random());
-          }
-        }
-      }
-
-      if (nachricht.indexOf(config.botName) > -1) {
-        if (nachricht.indexOf("wie geht") == -1) {
-
-          if (laune < 0) {
-            sleep.sleep(2);
-            bot.say(ziel, antwortboese);
-          } else {
-            sleep.sleep(2);
-            bot.say(ziel, antwortlieb);
-          }
-        } else {
-          sleep.sleep(2);
-          bot.say(ziel, verbalLaune(laune));
-          laune = laune - 5;
-        }
-      }
+      greeting = (greeting + "SIE ").toUpperCase();
+      onlineRequests.getRandomWikiTitle(greeting, function(prefix, title) {
+          sleep.sleep(1);
+          title = title.toUpperCase();
+          bot.say(to, prefix + title + '!');
+          console.log(prefix + title + '!');
+          });
+    }
   }
 }
 
+function getDrachenQuote(callback) {
+  var index = Math.floor((Math.random() * (drachenlord.list.length)));  
+  var title = drachenlord.list[index];
+  callback(title);
+}
+
+function getBoa(callback) {
+  var index = Math.floor((Math.random() * (boa.list.length)));  
+  var title = boa.list[index].toUpperCase();
+  callback(title);
+}
+
+function getWarning(callback) {
+  var warningNum = (Math.floor(Math.random()*warnings.list.length));
+  var warning = "ACHTYNG: " + warnings.list[warningNum];
+  sleep.sleep(2);
+  callback(warning);
+}
+
+function reagieren(absender, ziel, nachricht) {
+  var regex = '^!begruesze (.*)';
+  var result = nachricht.match(regex);
+  if(result != null) {
+    greeting(ziel, result[1]);
+  } else if(nachricht.startsWith("!wetter")) {
+    getBoa(function(title) {
+      weather.getWeather(absender, title, nachricht, function(msg) {
+          bot.say(ziel, msg);
+      });
+    });
+  } else if(nachricht.startsWith("!drachenlord")) {
+    getDrachenQuote(function(title) {
+      bot.say(ziel, absender+ ', '+ title);
+    });
+  } else if(nachricht.startsWith("!warnhinweysz")) {
+      getWarning(function(warning) {
+        bot.say(ziel, warning);
+      });
+  } else if(isPhrase(ziel, nachricht)) {
+  } else {
+    drama.dramaFunc(absender, config.botName, nachricht, function(msg) {
+      bot.say(ziel, msg);
+    });
+  }
+}
 
 bot.addListener("topic", function(channel, topic, nick, message) {
     console.log("Topic for", channel + " is", topic);
-    var gruss = gruesse[Math.floor(Math.random() * gruesse.length)];
-    bot.say(channel, gruss);
+    drama.join(function(msg) {
+      bot.say(channel, msg);
+    });
 });
 
 bot.addListener("error", function(message) {
@@ -259,19 +132,23 @@ bot.addListener("error", function(message) {
 });
 
 bot.addListener("join", function(channel, who) {
-    begruessen(channel, who); 
+    drama.begruessen(who, config.botName, function(msg) {
+      bot.say(channel, msg);
+    }); 
 });
 
 bot.addListener("message", function(from, to, text, message) {
-    reagieren(to,text); 
+    reagieren(from, to,text); 
 });
 
 bot.addListener("kick", function(channel, nick, by, reason, message) {
-    verabschieden(channel);
-    laune = laune + 5;
+  drama.verabschieden(function(bye) {
+    bot.say(channel, bye);
+  });
 });
 
 bot.addListener("quit", function(nick, reason, channels, message) {
-    verabschieden(channels);
+  drama.verabschieden(function(bye) {
+    bot.say(channels, bye);
+  });
 });
-
